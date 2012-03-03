0e1d0927fae12a12770ddffaafa6c006e5243b29
Israel Fermin Montilla <iferminm@gmail.com>
2012-03-03 17:56:19 -0430

    finished the translation logic

--- translator.py
+++ translator.py
@@ -1,6 +1,5 @@
 # -*- encoding: utf-8 -*-
 # Programmed by: israelord <iferminm at gmail dot com>
-
 import lex_parser as parser
 
 class Translator:
@@ -16,8 +15,9 @@ class Translator:
             'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
             'owl': 'http://www.w3.org/2002/07/owl#',
         }
+        self.condition_lists = []
 
-    def get_parsed_tree(self, complete_query):
+    def __get_parsed_tree(self, complete_query):
         """
         Calculates the query's syntactic tree
         according to the grammar defined on the
@@ -25,5 +25,39 @@ class Translator:
         """
         return parser.parse(complete_query)
     
-    def proccess_tree(self, tree):
-        pass
+    def __proccess_and(self, sub_tree, result):
+        if type(sub_tree) == str:
+            condition = self.prefixes['thesis'] + 'has-annotation'
+            value = self.prefixes['thesis'] + sub_tree
+            result.append("?s <%s> <%s>" % (condition, value))
+            return result
+        else:
+            result = self.__proccess_and(sub_tree[1], result)
+            result = self.__proccess_and(sub_tree[2], result)
+            return result
+
+    def __proccess_tree(self, tree):
+        if (tree[0] == "||"):
+            self.__proccess_tree(tree[1])
+            self.__proccess_tree(tree[2])
+        elif (tree[0] == "&&"):
+            result = []
+            result = self.__proccess_and(tree[1], [])
+            result.extend(self.__proccess_and(tree[2], []))
+            self.condition_lists.append(result)
+        elif (tree[0] == "?rel:"):
+            print "rel\n"
+        else:
+            self.condition_lists.append([tree])
+
+    def main(self, query):
+        tree = self.__get_parsed_tree(query)
+        self.__proccess_tree(tree)
+        for con in self.condition_lists:
+            print ' . '.join(con)
+
+if __name__ == '__main__':
+    while True:
+        t = Translator()
+        complete_query = raw_input("query>")
+        t.main(complete_query)
